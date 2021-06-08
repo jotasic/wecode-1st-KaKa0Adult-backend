97e5d98f34e241eee8a572c74aecc2a25d470ba0
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-06-08 21:19:23 +0900

    Create: users,products,orders app 및 모델 추가

---
+++ orders/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'order_statuses'
+
+class Order(models.Model):
+    user         = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    order_time   = models.DateTimeField(null=True)
+    order_status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'orders'
+
+class OrderList(models.Model):
+    order   = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    count   = models.IntegerField()
+
+    class Meta:
+        db_table = 'order_lists'
+    
+class RecipientInfo(models.Model):
+    address      = models.CharField(max_length=100)
+    name         = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=15)
+    request      = models.CharField(max_length=100)
+    order        = models.OneToOneField(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'recipient_infoes'
