a2e2aa322f590259e57bbaa39762863ef1692cb1
Taewoo Kim <embedded61@gmail.com>
2021-06-11 13:55:00 +0900

    Add: Product 조회 기능 추가
    - 쿼리파라미터에 따라서 상품들의 리스트를 응답으로 줄 수 있도록 기능 구현
    - 무한스크롤 기능을 대비하여 페이지 번호 및 페이지 사이즈를 받아서
      처리할 수 있도록 하였으며, 해당 파라미터가 없을 시, 키워드에 대한 모든
    정보를 응답하도록 함.
    
    예시)
    /products?type=타입명&keyword=키워드&pageSize=페이지사이즈&page=페이지번호
    - type : 필터링 타입 new,hot,search,category,character가 값으로 올 수
      있음
    - keyword : 필터링 타입에 따른 검색 키워드
    - pageSize : 페이지 사이즈 한번 요청시 받아올 결과 갯수
    - page : 현재 페이지
    - pageSize 와 page를 포함 시키지 않으면 전체 결과를 반환함
    
    요청 결과 예시(백 -> 프)
    {
        "resultList": [
            {
                "id": 90,
                "name": "[마지막기회] 신소재 앵무새 집",
                "price": "82500.000",
                "stock": true,
                "like": false,
                "cart": false,
                "image": "https://i.picsum.photos/id/866/256/256.jpg?ramdom=1"
            },
            {
                "id": 89,
                "name": "[마지막기회] 신소재 고슴도치 집",
                "price": "83100.000",
                "stock": true,
                "like": false,
                "cart": false,
                "image": "https://i.picsum.photos/id/866/256/256.jpg?ramdom=1"
            }
        ],
        "page": 1,
        "pageSize": 2,
        "totalCount": 90,
        "totalPageCount": 45,
        "numberOfElements": 2
    }
    
    1. resultList : 결과 리스트
        - id : 상품 id값
        - name : 상품 이름
        - price : 상품 가격
        - stock : 재고 여부
        - like : 좋아요 여부
        - cart : 장바구니 여부
        - image : 대표이미지 url
    
    2.  page : 현재 페이지 번호
    3. pageSize : 페이지 한개당 결과 갯수
    4. totalCount : 전체 아이템 갯수
    5. totalPageCount : 전체 페이지 갯수
    6. numberOfElements : 결과 리스트의 길이

--- products/views.py
+++ products/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+import json, math
 
-# Create your views here.
+from django.db.models.expressions import Case, When
+from django.db.models.query       import QuerySet
+from django.http.response         import JsonResponse
+from django.views                 import View
+from django.db.models             import Count
+
+from products.models              import Product
+from users.models                 import Like
+
+def filter_in_preserve(queryset: QuerySet, field: str, values: list) -> QuerySet:
+    preserved = Case(*[When(**{field : val}, then=pos) 
+                    for pos, val in enumerate(values)])
+    return queryset.filter(**{f'{field}__in': values}).order_by(preserved)
+
+class ProductView(View):
+    def get(self, request):
+
+        user_id = 1
+        product_type = request.GET.get('type', '')
+        keyword      = request.GET.get('keyword', '')
+        page         = int(request.GET.get('page', 0))
+        page_size    = int(request.GET.get('pageSize', 0))
+                
+        if product_type == 'new':
+            filtered_products = Product.objects.order_by('-created_at')
+
+        elif product_type == 'hot':
+            ordered_product_id = list(Like.objects
+                .values_list('product', flat=True)
+                .annotate(count=Count('product')).order_by('-count'))
+
+            filtered_products = filter_in_preserve(
+                Product.objects, 
+                'id', 
+                ordered_product_id).all()
+
+        elif product_type == 'category':
+            filtered_products = Product.objects.filter(
+                category__name__icontains=keyword).order_by('-created_at')
+
+        elif product_type == 'character':
+            filtered_products = Product.objects.filter(
+                character__name__icontains=keyword).order_by('-created_at')
+
+        elif product_type == 'search':
+            filtered_products = Product.objects.filter(
+                name__icontains=keyword).order_by('-created_at')
+
+        elif product_type == "":
+            filtered_products = Product.objects.all().order_by('-created_at')
+
+        else:
+            filtered_products = Product.objects.none()
+
+        total_count          = filtered_products.count()
+        result               = {}
+        result['totalCount'] = total_count
+
+        if page > 0 and page_size > 0:
+            started_idx       = (page - 1) * page_size
+            ended_idx         = started_idx + page_size
+            filtered_products = filtered_products[started_idx:ended_idx]
+            
+            result['page']           = page
+            result['pageSize']       = page_size
+            result['totalPageCount'] = math.ceil(total_count / page_size)
+        
+        result['numberOfElements'] = filtered_products.count()
+        result['resultList']       = [{
+            'id'    : product.id,
+            'name'  : product.name,
+            'price' : product.price,
+            'stock' : product.stock > 0,
+            'like'  : product.user_set.filter(id=user_id).exists(),
+            'cart'  : product.orderlist_set.all().filter(
+                order__user__id=user_id,
+                order__order_status__status='BASKET').exists(),
+            'image' : product.imageurl_set.order_by('id')[0].url
+                } for product in filtered_products]
+                
+        return JsonResponse(result, status=200)
