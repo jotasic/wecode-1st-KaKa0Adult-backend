a0190246ef4f5cdcf2dd7d0f02b8b5c181fb7000
Taewoo Kim <embedded61@gmail.com>
2021-06-14 20:30:14 +0900

    Add: 제품상세페이지 결과 값에 별점을 준 유저가 몇명인지의 대한 data도 추가

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.http.response        import Http404, JsonResponse
 from django.views                import View
 from django.db.models            import Value
 from django.db.models.functions  import Coalesce
-from django.db.models.aggregates import Avg
+from django.db.models.aggregates import Count, Avg
 from django.shortcuts            import get_object_or_404
 
 from products.models             import Product
@@ -14,19 +14,24 @@ class ProductDetailView(View):
         try:
             product = get_object_or_404(Product, id=product_id)
             user    = request.user
+
+            sart_point_info = product.review_set.aggregate(
+                    avg=Coalesce(Avg('star_point'), Value(0.0)),
+                    count=Count('id'))
+
             result  = {
-                'id'        : product.id,
-                'name'      : product.name,
-                'price'     : float(product.price),
-                'content'   : product.content,
-                'stock'     : product.stock > 0,
-                'starPoint' : product.review_set.aggregate(
-                    avg=Coalesce(Avg('star_point'), Value(0.0)))['avg'],
-                'imageUrls' : list(
+                'id'             : product.id,
+                'name'           : product.name,
+                'price'          : product.price,
+                'content'        : product.content,
+                'stock'          : product.stock,
+                'starPoint'      : sart_point_info['avg'],
+                'starPointCount' : sart_point_info['count'],
+                'imageUrls'      : list(
                     product.imageurl_set.values_list(
                     'url', flat=True).order_by('id')),
-                'like'      : product.user_set.filter(id=user.id).exists(),
-                'cart'      : product.orderlist_set.all().filter(
+                'like'           : product.user_set.filter(id=user.id).exists(),
+                'cart'           : product.orderlist_set.all().filter(
                     order__user__id             = user.id,
                     order__order_status__status = 'BASKET').exists(),
             }
