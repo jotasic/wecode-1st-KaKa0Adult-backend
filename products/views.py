8d48fd0e0a3afe79632560037080226cfd919b38
Taewoo Kim <embedded61@gmail.com>
2021-06-15 08:25:49 +0900

    Refactor: 상품목록 필터링 코드 수정
    1. Q객체를 담는 변수명 암묵적인룰에 맞게 q로 변경
    2. 최신순으로 정렬하는 옵션인 -created_at을 모든 조건에서 사용하므로 각
       if문에서 빼고, 공통으로 추가하도록 수정
    3. annotate 조건은 hot에서 정렬에 따라서 사용하므로 조건을 변수에 담지
       않고, 바로 사용할 수 있도록 수
    4. 그외 불필요한 구문 수정(user_id > user, .all().filter() > filter())

--- products/views.py
+++ products/views.py
@@ -13,41 +13,33 @@ from users.utils                  import login_decorator
 class ProductView(View):
     @login_decorator
     def get(self, request):
-        user                = request.user
-        product_type        = request.GET.get('type')
-        character           = request.GET.get('character')
-        search              = request.GET.get('search')
-        category            = request.GET.get('category')
-        page                = int(request.GET.get('page', 0))
-        page_size           = int(request.GET.get('pageSize', 0))
-        order_conditions    = []
-        annotate_conditions = {}
-        filter_conditions   = Q()
-        result              = {}
-
-        if product_type == 'new':
-            order_conditions.append('-created_at')
+        user             = request.user
+        product_type     = request.GET.get('type')
+        character        = request.GET.get('character')
+        search           = request.GET.get('search')
+        category         = request.GET.get('category')
+        page             = int(request.GET.get('page', 0))
+        page_size        = int(request.GET.get('pageSize', 0))
+        order_conditions = []
+        q                = Q()
+        result           = {}
 
         if product_type == 'hot':
-            annotate_conditions['count'] = Count("like__product_id")
             order_conditions.append('-count')
-            order_conditions.append('-created_at')
 
         if category:
-            filter_conditions |= Q(category__name__icontains=category)
-            order_conditions.append('-created_at') 
+            q |= Q(category__name__icontains=category)
 
         if character:
-            filter_conditions |= Q(character__name__icontains=character)
-            order_conditions.append('-created_at') 
+            q |= Q(character__name__icontains=character)
 
         if search:
-            filter_conditions |= Q(name__icontains=search)
-            order_conditions.append('-created_at')
+            q |= Q(name__icontains=search)
+
+        order_conditions.append('-created_at')
         
         filtered_products = Product.objects.filter(
-            filter_conditions).annotate(
-                **annotate_conditions).order_by(
+            q).annotate(count=Count("like__product_id")).order_by(
                 *order_conditions)
         
         total_count = filtered_products.count()
@@ -69,8 +61,8 @@ class ProductView(View):
             'price' : product.price,
             'stock' : product.stock,
             'like'  : product.user_set.filter(id=user.id).exists(),
-            'cart'  : product.orderitem_set.all().filter(
-                order__user__id=user.id,
+            'cart'  : product.orderitem_set.filter(
+                order__user=user,
                 order__order_status__status=OrderStatus.BASKET).exists(),
             'image' : product.imageurl_set.order_by('id')[0].url
                 } for product in filtered_products]
