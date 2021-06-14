6dd03a12429e9e0cd47efda0f58393dfe444c094
Taewoo Kim <embedded61@gmail.com>
2021-06-14 20:05:27 +0900

    Refactor: 조건에 맞게 상품목록을 검색하는 부분 수정
    - 기존 : type에 맞게 if문 안에서 일일이 filter를 이용해서 쿼리셋을
      받아냄
    - 변경 : Q객체, List, dic를 이용해서 검색할 조건들을 취합하고 마지막에
      하나의 filter문으로 처리되도록 수정함.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 import json, math
+from django.db.models.aggregates import Aggregate
+from django.db.models.query_utils import Q
 
 from django.http.response         import JsonResponse
 from django.views                 import View
@@ -10,57 +12,61 @@ from users.utils                  import login_decorator
 class ProductView(View):
     @login_decorator
     def get(self, request):
-        user         = request.user
-        product_type = request.GET.get('type', '')
-        keyword      = request.GET.get('keyword', '')
-        page         = int(request.GET.get('page', 0))
-        page_size    = int(request.GET.get('pageSize', 0))
-                
+        user                = request.user
+        product_type        = request.GET.get('type')
+        character           = request.GET.get('character')
+        search              = request.GET.get('search')
+        category            = request.GET.get('category')
+        page                = int(request.GET.get('page', 0))
+        page_size           = int(request.GET.get('pageSize', 0))
+        order_conditions    = []
+        annotate_conditions = {}
+        filter_conditions   = Q()
+        result              = {}
+
         if product_type == 'new':
-            filtered_products = Product.objects.order_by('-created_at')
+            order_conditions.append('-created_at')
 
-        elif product_type == 'hot':
-            filtered_products = Product.objects.annotate(
-                count=Count('like__product_id'
-                )).order_by('-count', '-id')
+        if product_type == 'hot':
+            annotate_conditions['count'] = Count("like__product_id")
+            order_conditions.append('-count')
+            order_conditions.append('-created_at')
 
-        elif product_type == 'category':
-            filtered_products = Product.objects.filter(
-                category__name__icontains=keyword).order_by('-created_at')
+        if category:
+            filter_conditions |= Q(category__name__icontains=category)
+            order_conditions.append('-created_at') 
 
-        elif product_type == 'character':
-            filtered_products = Product.objects.filter(
-                character__name__icontains=keyword).order_by('-created_at')
+        if character:
+            filter_conditions |= Q(character__name__icontains=character)
+            order_conditions.append('-created_at') 
 
-        elif product_type == 'search':
-            filtered_products = Product.objects.filter(
-                name__icontains=keyword).order_by('-created_at')
+        if search:
+            filter_conditions |= Q(name__icontains=search)
+            order_conditions.append('-created_at')
+        
+        filtered_products = Product.objects.filter(
+            filter_conditions).annotate(
+                **annotate_conditions).order_by(
+                *order_conditions)
+        
+        total_count = filtered_products.count()
 
-        elif product_type == "":
-            filtered_products = Product.objects.all().order_by('-created_at')
-
-        else:
-            filtered_products = Product.objects.none()
-
-        total_count          = filtered_products.count()
-        result               = {}
-        result['totalCount'] = total_count
-
-        if page > 0 and page_size > 0:
+        if page > 0 or page_size > 0:
             started_idx       = (page - 1) * page_size
             ended_idx         = started_idx + page_size
             filtered_products = filtered_products[started_idx:ended_idx]
-            
+
             result['page']           = page
             result['pageSize']       = page_size
             result['totalPageCount'] = math.ceil(total_count / page_size)
-        
+
+        result['totalCount']       = total_count
         result['numberOfElements'] = filtered_products.count()
         result['resultList']       = [{
             'id'    : product.id,
             'name'  : product.name,
             'price' : product.price,
-            'stock' : product.stock > 0,
+            'stock' : product.stock,
             'like'  : product.user_set.filter(id=user.id).exists(),
             'cart'  : product.orderlist_set.all().filter(
                 order__user__id=user.id,
