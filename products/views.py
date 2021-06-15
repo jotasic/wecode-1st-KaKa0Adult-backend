2fadb4f253a33e3cb06087db94efc32d7bb8730f
Taewoo Kim <embedded61@gmail.com>
2021-06-15 15:50:45 +0900

    Refactor: 검색 필터링 로직 최적화 (진행중)

--- products/views.py
+++ products/views.py
@@ -13,34 +13,49 @@ from users.utils                  import login_decorator
 class ProductView(View):
     @login_decorator
     def get(self, request):
-        user             = request.user
-        product_type     = request.GET.get('type')
-        character        = request.GET.get('character')
-        search           = request.GET.get('search')
-        category         = request.GET.get('category')
-        page             = int(request.GET.get('page', 0))
-        page_size        = int(request.GET.get('pageSize', 0))
-        order_conditions = []
-        q                = Q()
-        result           = {}
+        order_columns = {
+            'createdAt' : 'created_at',
+            'like'      : 'count',
+            'price'     : 'price',
+        }
 
-        if product_type == 'hot':
-            order_conditions.append('-count')
+        order_sort = {
+            'desc' : '-',
+            'asc'  : ''
+        }
+
+        user      = request.user
+        character = request.GET.get('character')
+        search    = request.GET.get('search')
+        category  = request.GET.get('category')
+        order     = request.GET.get('order')
+        page      = int(request.GET.get('page', 0))
+        page_size = int(request.GET.get('pageSize', 0))
+        q         = Q()
+        result    = {}
+
+        conditions = {
+            'category'  : Q(category__name__icontains=category),
+            'character' : Q(character__name__icontains=character),
+            'search'    : Q(name__icontains=search)
+        }
 
         if category:
-            q |= Q(category__name__icontains=category)
+            q &= conditions['category']
 
         if character:
-            q |= Q(character__name__icontains=character)
+            q &= conditions['character']
 
         if search:
-            q |= Q(name__icontains=search)
+            q &= conditions['search']
 
-        order_conditions.append('-created_at')
-        
+        if order:
+            order = order.split(',')
+            order = order_sort[order[1]]+order_columns[order[0]]
+
         filtered_products = Product.objects.filter(
             q).annotate(count=Count("like__product_id")).order_by(
-                *order_conditions)
+                order)
         
         total_count = filtered_products.count()
 
@@ -49,22 +64,23 @@ class ProductView(View):
             ended_idx         = started_idx + page_size
             filtered_products = filtered_products[started_idx:ended_idx]
 
-            result['page']           = page
-            result['pageSize']       = page_size
-            result['totalPageCount'] = math.ceil(total_count / page_size)
-
-        result['totalCount']       = total_count
-        result['numberOfElements'] = filtered_products.count()
-        result['resultList']       = [{
-            'id'    : product.id,
-            'name'  : product.name,
-            'price' : product.price,
-            'stock' : product.stock,
-            'like'  : product.user_set.filter(id=user.id).exists(),
-            'cart'  : product.orderitem_set.filter(
-                order__user            = user,
-                order__order_status_id = OrderStatus.BASKET).exists(),
-            'image' : product.imageurl_set.order_by('id')[0].url
+        result = {   
+            'totalCount'      : total_count,
+            'numberOfElements': filtered_products.count(),
+            'page'            : page,
+            'pageSize'        : page_size,
+            'totalPageCount'  : math.ceil(total_count / page_size) if page_size > 0 else 0,
+            'resultList'      : [{
+                'id'    : product.id,
+                'name'  : product.name,
+                'price' : product.price,
+                'stock' : product.stock,
+                'like'  : product.user_set.filter(id=user.id).exists(),
+                'cart'  : product.orderitem_set.filter(
+                    order__user            = user,
+                    order__order_status_id = OrderStatus.BASKET).exists(),
+                'image' : product.imageurl_set.order_by('id')[0].url
                 } for product in filtered_products]
+        }
                 
         return JsonResponse(result, status=200)
