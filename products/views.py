e9f917ddffac7eddf9ac5dda0870a1663cda3f38
Taewoo Kim <embedded61@gmail.com>
2021-06-15 16:00:04 +0900

    Modify: 컨벤션 수정

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.db.models.query_utils import Q
 from django.http.response         import Http404, JsonResponse
 from django.views                 import View
 from django.db.models             import Count, Avg
-from django.shortcuts            import get_object_or_404
+from django.shortcuts             import get_object_or_404
 
 from products.models              import Product
 from orders.models                import OrderStatus
