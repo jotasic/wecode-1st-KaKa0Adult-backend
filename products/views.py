542d348aa08accb2fb2d683185070bdd023c579a
Taewoo Kim <embedded61@gmail.com>
2021-06-16 12:59:51 +0900

    Modify: 제품 상세페이지 결과 양식 변경  - 프론트에서 제품 리스트 결과와 동일하게 List에 담아서 달라고 요청하여    변경

--- products/views.py
+++ products/views.py
@@ -18,22 +18,23 @@ class ProductDetailView(View):
                     avg=Avg('star_point'), count=Count('id'))
 
             result  = {
-                'id'             : product.id,
-                'name'           : product.name,
-                'price'          : product.price,
-                'content'        : product.content,
-                'stock'          : product.stock,
-                'starPoint'      : sart_point['avg'] if sart_point['avg'] else 0.0,
-                'starPointCount' : sart_point['count'],
-                'imageUrls'      : list(
-                    product.imageurl_set.values_list(
-                    'url', flat=True).order_by('id')),
-                'like'           : product.user_set.filter(id=user.id).exists(),
-                'cart'           : product.orderitem_set.filter(
-                    order__user            = user,
-                    order__order_status_id = OrderStatus.BASKET).exists(),
-            }
-
+                'resultList' : [{
+                    'id'             : product.id,
+                    'name'           : product.name,
+                    'price'          : product.price,
+                    'content'        : product.content,
+                    'stock'          : product.stock,
+                    'starPoint'      : sart_point['avg'] if sart_point['avg'] else 0.0,
+                    'starPointCount' : sart_point['count'],
+                    'imageUrls'      : list(
+                        product.imageurl_set.values_list(
+                        'url', flat=True).order_by('id')),
+                    'like'           : product.user_set.filter(id=user.id).exists(),
+                    'cart'           : product.orderitem_set.filter(
+                        order__user            = user,
+                        order__order_status_id = OrderStatus.BASKET).exists(),
+            }]}
+            
             return JsonResponse(result, status=200)
 
         except Http404:
