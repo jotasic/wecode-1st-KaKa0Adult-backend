49135155342e01e2263bf1b59a7c792e3e0b360b
이정민 <dlwjdals5600@gmail.com>
2021-06-16 15:52:50 +0900

    Add: 사용자가 좋아요 한 상품들의 리스트 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LoginView, SignupView, LikeView
+from .views import LoginView, SignupView, LikeView, UserLikeView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/login', LoginView.as_view()),
     path('/like/product/<int:product_id>', LikeView.as_view()),
     path('/like/product', LikeView.as_view()),
+    path('/like/list', UserLikeView.as_view()),
 ]
