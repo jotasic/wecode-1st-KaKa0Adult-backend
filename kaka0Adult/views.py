72a1b1fcaa169978e8bd9fe9df0a3ea1bc5bbad6
TaewooKim <embedded61@gmail.com>
2021-07-29 14:40:05 +0900

    발표

--- kaka0Adult/views.py
+++ kaka0Adult/views.py
@@ -3,4 +3,18 @@ from kaka0Adult.settings.base import get_aws_linux_ec2_private_ip
 
 def index(request):
      ip = get_aws_linux_ec2_private_ip()
-     return HttpResponse(f"Hello, world\nyour private ip is {ip}")
+     return HttpResponse(f"""
+ <!DCOTYPE html>
+    <html lang="ko">
+
+    <head>
+        <title>Wecode 21기 고생많으셨습니다. </title>
+    </head>
+
+    <body>
+          <H1> 21기 여러분 3개월동안 고생많으셨습니다!! 👍</H1>
+    </body>
+
+    </html>
+     """
+     )
